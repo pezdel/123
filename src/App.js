a7f165adf9c132edcabc8d07764838273bd22151
pezdel <adam.pezdel@gmail.com>
2021-06-05 13:04:06 -0500

    base

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Chart from './ts/chart'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>skldfj</h1>
+      <Chart />
+      <h1>skldjfsas</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ts/chart.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Chart()
+{    return (
+      <>
+          <p>This is ffa ssss ssssaaaecaaation</p>
+      </>
+  );}
